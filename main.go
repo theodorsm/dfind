e28d4d911e72de0144309d7a967668bd08668a5a
theodorsm <theodor@midtlien.com>
2024-02-12 17:13:43 +0100

    Refactor printing

--- main.go
+++ main.go
@@ -37,6 +37,9 @@ const OffsetMajorVersion = 0x19
 const OffsetMinorVersion = 0x1a
 const OffsetSessionLength = 0x3b
 
+const ClientHelloType = 0x1
+const ServerHelloType = 0x2
+
 func addFingerprint(db *sql.DB, fp Fingerprint) error {
 	result, err := db.Exec("INSERT INTO fingerprint (type, handshakeType, length, fragmentOffset, majorVersion, minorVersion, cipherLength, ciphers, chosenCipher, extensionLength, extensions) VALUES ('snowflake', ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", fp.HandshakeType, fp.Length, fp.FragmentOffset, int(fp.MajorVersion), int(fp.MinorVersion), fp.CipherLength, hex.EncodeToString(fp.Ciphers), hex.EncodeToString(fp.ChosenCipher), fp.ExtensionLength, hex.EncodeToString(fp.Extensions))
 	if err != nil {
@@ -48,6 +51,31 @@ func addFingerprint(db *sql.DB, fp Fingerprint) error {
 		return fmt.Errorf("addFingerprint: %v", err)
 	}
 	return nil
+}
+
+func printFingerprint(fp Fingerprint) {
+	fmt.Println("=============")
+	switch fp.HandshakeType {
+	case ClientHelloType:
+		fmt.Println("Client Hello:")
+		fmt.Printf("Length: %d\n", fp.Length)
+		fmt.Printf("Fragment Offset: %d\n", fp.FragmentOffset)
+		fmt.Printf("DTLS v%d.%d\n", 0xff-fp.MajorVersion, 0xff-fp.MinorVersion)
+		fmt.Printf("Cipher suite length: %d\n", fp.CipherLength)
+		fmt.Printf("Ciphers: %x\n", fp.Ciphers)
+		fmt.Printf("Extension length: %d\n", fp.ExtensionLength)
+		fmt.Printf("Extensions: %x\n", fp.Extensions)
+	case ServerHelloType:
+		fmt.Println("Server Hello:")
+		fmt.Printf("Length: %d\n", fp.Length)
+		fmt.Printf("Fragment Offset: %d\n", fp.FragmentOffset)
+		fmt.Printf("DTLS v%d.%d\n", 0xff-fp.MajorVersion, 0xff-fp.MinorVersion)
+		fmt.Printf("Chosen cipher suite: %x\n", fp.ChosenCipher)
+		fmt.Printf("Extension length: %d\n", fp.ExtensionLength)
+		fmt.Printf("Extensions: %x\n", fp.Extensions)
+	default:
+		return
+	}
 }
 
 func main() {
@@ -97,60 +125,32 @@ func main() {
 			fp.HandshakeType = uint(dtls[OffsetHandshakeType])
 
 			switch fp.HandshakeType {
-			case 0x1:
-				fmt.Println("=============")
-				fmt.Println("Client Hello:")
-
+			case ClientHelloType:
 				fp.Length = uint(dtls[OffsetLength+2]) | uint(dtls[OffsetLength+1])<<8 | uint(dtls[OffsetLength])<<16
-				fmt.Printf("Length: %d\n", fp.Length)
-
 				fp.FragmentOffset = uint(dtls[OffsetFragmentOffset]) | uint(dtls[OffsetFragmentOffset+1])<<8 | uint(dtls[OffsetFragmentOffset+2])<<16
-				fmt.Printf("Fragment Offset: %d\n", fp.FragmentOffset)
-
 				fp.MajorVersion = dtls[OffsetMajorVersion]
 				fp.MinorVersion = dtls[OffsetMinorVersion]
-				fmt.Printf("DTLS v%d.%d\n", 0xff-fp.MajorVersion, 0xff-fp.MinorVersion)
-
 				OffsetCookieLength := OffsetSessionLength + int(dtls[OffsetSessionLength]) + 1
 				OffsetCipherLength := OffsetCookieLength + int(dtls[OffsetCookieLength]) + 1
 				fp.CipherLength = uint(dtls[OffsetCipherLength+1]) | uint(dtls[OffsetCipherLength])<<8
-				fmt.Printf("Cipher suite length: %d\n", fp.CipherLength)
-
 				fp.Ciphers = dtls[OffsetCipherLength+2 : OffsetCipherLength+2+int(fp.CipherLength)]
-				fmt.Printf("Ciphers: %x\n", fp.Ciphers)
-
 				OffsetCompressionLength := OffsetCipherLength + 2 + int(fp.CipherLength) + 1
 				OffsetExtensionLength := OffsetCompressionLength + int(dtls[OffsetCompressionLength]) + 1
 				fp.ExtensionLength = uint(dtls[OffsetExtensionLength+1]) | uint(dtls[OffsetExtensionLength])<<8
-				fmt.Printf("Extension length: %d\n", fp.ExtensionLength)
-
 				fp.Extensions = dtls[OffsetExtensionLength+2 : OffsetExtensionLength+2+int(fp.ExtensionLength)]
-				fmt.Printf("Extensions: %x\n", fp.Extensions)
+				printFingerprint(fp)
 				err := addFingerprint(db, fp)
 				if err != nil {
 					fmt.Println(err)
 				}
-			case 0x2:
-				fmt.Println("=============")
-				fmt.Println("Server Hello:")
-
-				handshakeLength := uint(dtls[OffsetLength+2]) | uint(dtls[OffsetLength+1])<<8 | uint(dtls[OffsetLength])<<16
-				fmt.Printf("Length: %d\n", handshakeLength)
-
-				fmt.Printf("Fragment Offset: %d\n", uint(dtls[OffsetFragmentOffset])|uint(dtls[OffsetFragmentOffset+1])<<8|uint(dtls[OffsetFragmentOffset+2])<<16)
-
-				fmt.Printf("DTLS v%d.%d\n", 0xff-dtls[OffsetMajorVersion], 0xff-dtls[OffsetMinorVersion])
-
+			case ServerHelloType:
+				fp.Length = uint(dtls[OffsetLength+2]) | uint(dtls[OffsetLength+1])<<8 | uint(dtls[OffsetLength])<<16
 				OffsetChosenCipher := OffsetSessionLength + int(dtls[OffsetSessionLength]) + 1
 				fp.ChosenCipher = dtls[OffsetChosenCipher : OffsetChosenCipher+2]
-				fmt.Printf("Chosen cipher suite: %x\n", fp.ChosenCipher)
-
 				OffsetExtensionLength := OffsetChosenCipher + 3
 				fp.ExtensionLength = uint(dtls[OffsetExtensionLength+1]) | uint(dtls[OffsetExtensionLength])<<8
-				fmt.Printf("Extension length: %d\n", fp.ExtensionLength)
-
 				fp.Extensions = dtls[OffsetExtensionLength+2 : OffsetExtensionLength+2+int(fp.ExtensionLength)]
-				fmt.Printf("Extensions: %x\n", fp.Extensions)
+				printFingerprint(fp)
 				err := addFingerprint(db, fp)
 				if err != nil {
 					fmt.Println(err)
